4ec01183b74b5a1f5d842c0d3eb6591892bdd314
shipramargurate <shivanikumari22322@gmail.com>
2022-11-05 17:29:17 +0530

    [Shivani]: Added functionality of checking message and repond happy or sad mood

---
+++ src/com/bridgelabz/moodAnalyzer/MoodAnalyzer.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.moodAnalyzer;
+
+public class MoodAnalyzer {
+
+	// creating method named moodCheck having string return type and passing
+	// parameter of string type
+
+	public String moodCheck(String message) {
+		if (message.contains("SAD")) {
+			System.out.println("In sad mood");
+			return "SAD";
+		} else {
+			System.out.println("In happy mood");
+			return "HAPPY";
+		}
+	}
+}
---
+++ src/com/bridgelabz/moodAnalyzerTest/MoodAnalyzerTest.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.moodAnalyzerTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.bridgelabz.moodAnalyzer.MoodAnalyzer;
+
+public class MoodAnalyzerTest {
+
+	MoodAnalyzer moodAnalyze = new MoodAnalyzer();
+
+	@Test
+	public void if_MessageWasSad_ShouldReturnSad() {
+		String message = " I am in HAPPY mood ";
+		String mood = moodAnalyze.moodCheck(message);
+		Assertions.assertEquals("HAPPY", mood);
+	}
+
+}
